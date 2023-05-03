933cd4b3912e21649c6e55fa8fbc42c598df17ac
kimkiyong <fenderst0612@gmail.com>
2023-05-03 13:30:44 +0900

    feat: bash meta programming

--- shell/bash.sh
+++ shell/bash.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 echo "seq 4の出力から、odd_1, odd_3と、event_2, event_4というディレクトリを作る"
-seq 4 | awk '$1%2==0{print"mkdir -p tmp/even_"$1}$1%2{print"mkdir -p tmp/odd_"$1}' | bash
+# seq 4 \
+# | awk '$1%2==0{print"mkdir -p tmp/even_"$1}$1%2{print"mkdir -p tmp/odd_"$1}' \
+# | bash
+
+# seq 4 \
+# | awk '{print "mkdir tmp/"($1%2?"odd_": "even_")$1}' \
+# | bash
+
+seq 4 \
+| awk '{if($1%2){a="odd_"}else{a="even_"};print "mkdir tmp/"a$1}' \
+| bash
