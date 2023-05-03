6e943915866b5e851c6454ed27cd0905f524a81c
kimkiyong <fenderst0612@gmail.com>
2023-05-03 13:13:36 +0900

    wip: bash

--- shell/bash.sh
+++ shell/bash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "bashによるメタプログラミング"
-seq 4 | awk '{print"mkdir -p tmp/odd_"$1}' | bash
+echo "seq 4の出力から、odd_1, odd_3と、event_2, event_4というディレクトリを作る"
+seq 4 | awk '$1%2==0{print"mkdir -p tmp/even_"$1}$1%2{print"mkdir -p tmp/odd_"$1}' | bash
