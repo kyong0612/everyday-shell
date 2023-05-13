f4e2b5ccf27f6c290a282b3e8db46f6e152a77e0
kimkiyong <fenderst0612@gmail.com>
2023-05-13 18:23:17 +0900

    feat: extract log by awk

--- shell/qdata/9/extract-log.sh
+++ shell/qdata/9/extract-log.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 # 2016年12月24日21時台の最初のログから、2016年12月25日3時台の最初のログまで出力する
-cat ./log_range.log \
+echo "sedを使用したパターン" 
+time cat ./log_range.log \
 | sort -k 4 \
 | sed -n '/24\/Dec\/2016 21:..:../,/25\/Dec\/2016 03:..:../p'
 
 
+echo "awkを使用したパターン①"
+time cat log_range.log \
+| awk '/24\/Dec\/2016 21:..:../,/25\/Dec\/2016 03:..:../'
+
+echo "awkを使用したパターン②"
+time cat log_range.log \
+| awk '$4" "$5>="[24/Dec/2016 21:00:00]" && $4" "$5<"[25/Dec/2016 03:59:60]"'
