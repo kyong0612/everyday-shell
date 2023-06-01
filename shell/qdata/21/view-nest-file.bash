36fdb7362aff0c3f8856a449b1a6e3875489dbbc
kimkiyong <fenderst0612@gmail.com>
2023-06-01 14:55:48 +0900

    chore: add script result

--- shell/qdata/21/view-nest-file.bash
+++ shell/qdata/21/view-nest-file.bash
@@ -4,6 +4,18 @@ echo "##### find + grep #####"
 find -type f | grep -v "\./dir_b/d" | sort
 
 
+# root@a75b77caf855:/shell/qdata/21# ./view-nest-file.bash
+# ##### find + grep #####
+# ./dir_a/file_1
+# ./dir_a/file_2
+# ./dir_b/file_1
+# ./dir_b/file_2
+# ./dir_c/dir_b/dir_a/file_1
+# ./dir_c/dir_b/file_1
+# ./dir_c/dir_b/file_2
+# ./view-nest-file.bash
+
+
 echo "##### use globstar ######"
 shopt -s globstar
 echo dir_a/* dir_b/* dir_c/** | grep -o "[^ ]*[0-]"
